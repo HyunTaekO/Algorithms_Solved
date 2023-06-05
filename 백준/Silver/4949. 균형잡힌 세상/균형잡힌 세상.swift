01540333697154d916bbeff253b9913a848da59e
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-06-05 16:14:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 28 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.swift
@@ -0,0 +1,23 @@
+var input = ""
+
+while true {
+    input = readLine()!
+    if input == "." { break }
+    var stack = [Character]()
+    var check = true
+    
+    for i in input {
+        if i == "[" || i == "(" { stack.append(i) }
+        else if i == "]" || i == ")" {
+            if stack.isEmpty { check = false; break }
+            if i == "]" && stack.removeLast() != "[" { check = false; break }
+            else if i == ")" && stack.removeLast() != "(" { check = false; break }
+        }
+    }
+    
+    if check == false { print("no") }
+    else {
+        if !stack.isEmpty { print("no") }
+        else {print("yes")}
+    }
+}
