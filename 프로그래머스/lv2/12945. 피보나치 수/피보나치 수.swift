d34e7d05ee3b6c480f046c14730063d2f733542c
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-06-08 03:04:52 +0900

    [level 2] Title: 피보나치 수, Time: 2.81 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.swift
@@ -0,0 +1,9 @@
+func solution(_ n:Int) -> Int {
+  var fibonacci: [Int] = [0, 1]
+  
+  for i in 2...n {
+    fibonacci.append((fibonacci[i-2] + fibonacci[i-1]) % 1234567)
+  }
+  
+  return fibonacci[n]
+}
