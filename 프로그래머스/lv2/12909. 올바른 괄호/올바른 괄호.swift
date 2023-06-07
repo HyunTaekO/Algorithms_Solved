fb91ce7eb483ddac05297b6a60bf88c6bce6f5b7
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-06-08 02:27:00 +0900

    [level 2] Title: 올바른 괄호, Time: 5.32 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var cnt = 0
+    for i in s {
+        if i == "(" {
+            cnt += 1
+        }else {
+            cnt -= 1
+        }
+        if cnt < 0 {
+            return false
+        }
+    }
+    
+    
+
+    return cnt == 0 ? true : false
+}
