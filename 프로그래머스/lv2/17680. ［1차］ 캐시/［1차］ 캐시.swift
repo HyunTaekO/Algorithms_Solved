819bfaecb4a938e259f31a071b843e81c6a34b6d
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-06-14 04:57:58 +0900

    [level 2] Title: [1차] 캐시, Time: 3.11 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -0,0 +1,23 @@
+func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+    guard cacheSize != 0 else{ return 5*cities.count }
+
+    var time = 0
+    var cache: [String] = []
+
+    for city in cities {
+        if let i = cache.index(of: city.lowercased()) {
+            cache.remove(at: i)
+            time += 1
+        } else {
+            if (cache.count >= cacheSize) {
+                cache.remove(at: 0)
+            }
+
+            time += 5
+        }
+
+        cache += [city.lowercased()]
+    }
+
+    return time
+}
