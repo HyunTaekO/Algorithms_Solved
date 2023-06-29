0788a49a8a6da51491d5014536f0258d2e9248d6
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-06-29 21:48:35 +0900

    [Silver III] Title: 바이러스, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -1,26 +1,29 @@
+import Foundation
+
 let computer = Int(readLine()!)!
-let connect = Int(readLine()!)!
-var node: [[Int]] = Array(repeating: [], count: computer + 1)
+let n = Int(readLine()!)!
+var netArr = Array(repeating: Array<Int>(), count: computer + 1)
+for _ in 0..<n {
+    let pair = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+    netArr[pair[0]].append(pair[1])
+    netArr[pair[1]].append(pair[0])
+}
 var visited = Array(repeating: false, count: computer + 1)
 
 func bfs() {
     var queue = [1]
-    visited[1] = true
+    
     while !queue.isEmpty {
-        let q = queue.removeFirst()
-        for i in node[q] {
+        let elem = queue.removeFirst()
+        
+        for i in netArr[elem] {
             if !visited[i] {
                 visited[i] = true
                 queue.append(i)
             }
         }
     }
+    
 }
-for _ in 1...connect {
-    let pair = readLine()!.split(separator: " ").map{ Int(String($0))!}
-    node[pair[0]].append(pair[1])
-    node[pair[1]].append(pair[0])
-}
-
 bfs()
-print(visited.filter{$0}.count - 1)
+print(visited.filter({$0}).count == 0 ? 0 : visited.filter({$0}).count - 1)
