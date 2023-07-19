1af829b34112f398bc8858f33f9816972c9b4bc2
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-07-19 10:00:12 +0900

    [Silver V] Title: 올림픽, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.swift
+++ 백준/Silver/8979. 올림픽/올림픽.swift
@@ -2,21 +2,23 @@ let nk = readLine()!.split(separator: " ").map{ Int(String($0))! }
 let n = nk[0]
 let k = nk[1]
 var country = Array(repeating: Array(repeating: 0, count: 4), count: n)
-var rank: [(Int, Int)] = []
+var rank = 1
 for i in 0..<n {
     let input = readLine()!.split(separator: " ").map{ Int($0)! }
-        
     country[i] = input
 }
-country = country.sorted(by: { ($0[1], $0[2], $0[3]) > ($1[1], $1[2], $1[3])})
-rank.append((country[0][0], 1))
-for c in 1..<country.count {
-    let now = country[c]
-    let before = country[c-1]
-    if now[1] == before[1] && now[2] == before[2] && now[3] == before[3] {
-        rank.append((now[0], rank[c-1].1))
-    }else {
-        rank.append((now[0], rank[c-1].1 + 1))
+if n == 1 {
+    print(rank)
+}else {
+    country = country.sorted(by: { ($0[1], $0[2], $0[3]) > ($1[1], $1[2], $1[3])})
+    for c in 1..<country.count {
+        let now = country[c]
+        let before = country[c-1]
+        if now[1] == before[1] && now[2] == before[2] && now[3] == before[3] {
+            
+        }else {
+            rank += 1
+        }
+        if now[0] == k { print(rank); break }
     }
-    if now[0] == k { print(rank[c].1); break }
 }
