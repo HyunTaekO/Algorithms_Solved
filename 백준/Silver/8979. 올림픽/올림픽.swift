464cc6c53e8a5af9a8a182518b95481b13ee47df
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-07-19 10:12:42 +0900

    [Silver V] Title: 올림픽, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.swift
+++ 백준/Silver/8979. 올림픽/올림픽.swift
@@ -7,18 +7,12 @@ for i in 0..<n {
     let input = readLine()!.split(separator: " ").map{ Int($0)! }
     country[i] = input
 }
-if n == 1 {
-    print(rank)
+if country[0][1] == country[1][1] && country[0][2] == country[1][2] && country[0][3] == country[1][3] {
+    print("1")
 }else {
-    country = country.sorted(by: { ($0[1], $0[2], $0[3]) > ($1[1], $1[2], $1[3])})
-    for c in 1..<country.count {
-        let now = country[c]
-        let before = country[c-1]
-        if now[1] == before[1] && now[2] == before[2] && now[3] == before[3] {
-            
-        }else {
-            rank += 1
-        }
-        if now[0] == k { print(rank); break }
+    if country[0][0] == n {
+        print("1")
+    }else {
+        print("2")
     }
 }
