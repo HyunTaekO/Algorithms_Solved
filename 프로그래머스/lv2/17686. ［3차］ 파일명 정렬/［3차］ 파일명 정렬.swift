ac0d2b4e7a02637f4fdb18294621a3339a3d32fe
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-07-31 01:05:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 48.82 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.swift
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.swift
@@ -1,5 +1,4 @@
 func solution(_ files:[String]) -> [String] {
-    let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     var splitFiles = files.map{ str -> [String] in 
                         let head = str.split(whereSeparator: {$0.isNumber}).map{ String($0) }[0] 
                         let number = str.split(whereSeparator: {$0.isLetter || $0 == "." || $0 == " " || $0 == "-"})
