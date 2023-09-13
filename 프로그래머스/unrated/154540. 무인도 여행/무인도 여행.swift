1e57512426804179cdeb9d1e72843cfb890bb8f6
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-09-13 12:47:21 +0900

    [unrated] Title: 무인도 여행, Time: 0.33 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+func solution(_ maps:[String]) -> [Int] {
+    var result = [Int]() // 각 섬에서 머무를 수 있는 최대 일 수 
+    let row = maps.count
+    let col = maps[0].count
+    let maps = maps.map{ $0.map{ String($0) }}
+    var visited = Array(repeating: Array(repeating: false, count: col), count: row)
+    let dx = [0, 0, -1, 1]
+    let dy = [1, -1, 0, 0]
+    
+    func bfs(x: Int, y: Int) -> Int {
+        visited[x][y] = true
+        guard maps[x][y] != "X" else{ return 0 }
+        var queue = [(x, y)]
+        var sum = 0
+        var idx = 0 
+        while idx < queue.count {
+            let (qx, qy) = queue[idx]
+            if maps[qx][qy] != "X" {
+                sum += Int(maps[qx][qy])!
+            }
+            
+            for i in 0..<4 {
+                let nx = qx + dx[i]
+                let ny = qy + dy[i]
+                guard 0..<row ~= nx && 
+                0..<col ~= ny && 
+                !visited[nx][ny] && 
+                maps[nx][ny] != "X" else { continue }
+                
+                visited[nx][ny] = true
+                queue.append((nx, ny))
+            }
+            idx += 1
+        }
+        return sum
+    }
+    
+    for x in 0..<row {
+        for y in 0..<col {
+            if !visited[x][y] {
+                let sum = bfs(x: x, y: y)
+                if sum > 0 {
+                    result.append(sum)
+                }
+            }
+        }
+    }
+    return result.isEmpty ? [-1] : result.sorted()
+}
+
