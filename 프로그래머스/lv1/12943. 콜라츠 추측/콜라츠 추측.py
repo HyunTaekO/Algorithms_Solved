fa3d3ee8ed1f3566470e98228011ee341de69744
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-10-03 20:37:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,13 @@
+def solution(num):
+    cnt = 0
+    if num == 1: return 0
+    while num > 1:
+        cnt += 1
+        if num % 2:
+            num = (num*3) + 1
+        else:
+            num //= 2
+        if cnt == 500: return -1
+        
+    return cnt
+
