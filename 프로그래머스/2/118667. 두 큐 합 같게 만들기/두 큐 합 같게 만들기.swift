1227040ea839be40382cdb2a3ebe7c791005d234
오현택 <111224287+HyunTaekO@users.noreply.github.com>
2023-11-21 15:50:08 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 30.50 ms, Memory: 25.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -1,31 +1,27 @@
 import Foundation
-// 1. 큐 두개를 합쳐 하나의 배열로 만든다. => 원소의 합이 더 큰 큐를 뒤로 보낸다.
-// 2. 초기 큐의 시작점과 끝점을 지정해 투포인터를 사용한다.
-// 3. 두 포인터 내 원소 합이 전체 합 / 2 보다 크면 시작점을 이동, 작으면 끝점을 이동시킨다.
-// 4. 두 포인터 내 원소 합 == 전체 합 / 2 될 때까지 찾는다.
-// 5. 4n 이상 수행했음에도 같아지지 않는다면 -1 리턴한다.
-// [1, 1, 1, 5]
+
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    let sumQ = queue1 + queue2
-    let half = sumQ.reduce(0, +) / 2
-    var pointer = (0, queue1.count-1)
-    var current = sumQ[pointer.0...pointer.1].reduce(0, +)
     var result = 0
-    while true {
+    let queues = queue1 + queue2
+    var sum = queue1.reduce(0, +)
+    let targetSum = (sum + queue2.reduce(0, +)) / 2
+    guard sum != targetSum else { return 0 }
+    var left = 0
+    var right = queue1.count - 1
+    while left < right {
+        result += 1
         
-        if current == half { return result }
-        
-        if current > half && pointer.0 + 1 <= pointer.1 {
-            current -= sumQ[pointer.0]
-            pointer.0 += 1
-        }else if pointer.1 + 1 < sumQ.count{
-            pointer.1 += 1
-            current += sumQ[pointer.1]
-        }else {
-            return -1
+        if sum < targetSum {
+            right += 1
+            if right == queues.count { break }
+            sum += queues[right]
+        }else if sum > targetSum {
+            sum -= queues[left]
+            left += 1
         }
         
-        result += 1
+        if sum == targetSum { return result }
+        
     }
-    return result
+    return -1
 }
